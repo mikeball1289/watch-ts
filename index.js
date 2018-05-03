1e492c3ec4ea074ed48162478c0b02443d985f9b
Mike Ball <mike.ball@mikutech.com>
2018-05-03 17:54:53 +0000

    support for multiple entry point compilation

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let path = require("path");
 let optimist = require("optimist");
 
 if (optimist.argv["init"]) {
+    let react = optimist.argv["init"] === "react";
     try {
         fs.statSync("tsconfig.json");
         console.error("tsconfig.json already exists");
@@ -15,6 +16,7 @@ if (optimist.argv["init"]) {
     fs.writeFileSync("tsconfig.json",
 `{
     "compilerOptions": {
+        ${react ? `"jsx": "react",` : ""}
         "target": "es6",
         "module": "commonjs",
         "noImplicitAny": true,
@@ -23,6 +25,7 @@ if (optimist.argv["init"]) {
         "inlineSources": true
     },
     "include": [
+        ${react ? `"src/**/*.tsx",` : ""}
         "src/**/*.ts"
     ]
 }`
@@ -32,50 +35,55 @@ if (optimist.argv["init"]) {
     process.exit(0);
 }
 
-let inFile = optimist.argv._[0];
+// let inFile = optimist.argv._[0];
 
-if (!inFile) {
+if (!optimist.argv._.length) {
     console.error("No compilation target specified");
     process.exit(1);
 }
-let outFile = optimist.argv.o;
-if (!outFile) {
-    outFile = inFile.replace(/\.tsx?$/, "") + ".js";
+let outPath = optimist.argv.o;
+if (!outPath) {
+    // outFile = inFile.replace(/\.tsx?$/, "") + ".js";
+    outPath = "";
 }
 
-let b = browserify( {
-    paths: [
-        "./node_modules"
-    ],
-    cache: {},
-    packageCache: {},
-    plugin: [ watchify ],
-    debug: true,
-} );
+for (let inFile of optimist.argv._) {
+    let outFile = path.basename(inFile).replace(/\.tsx?$/, "") + ".js";
 
-b.plugin(tsify, { project: "." } );
-b.add(inFile);
-
-b.on("update", bundle);
-bundle();
-
-function currentTime() {
-    let time = new Date();
-    return `${("0" + time.getHours()).slice(-2)}:${("0" + time.getMinutes()).slice(-2)}:${("0" + time.getSeconds()).slice(-2)}`
-}
-
-function bundle() {
-    let hadError = false;
-    let time = new Date();
-    console.log(`\x1b[32m[${currentTime()}]--- Beginning compilation\x1b[0m`);
-    b.bundle()
+    let b = browserify( {
+        paths: [
+            "./node_modules"
+        ],
+        cache: {},
+        packageCache: {},
+        plugin: [ watchify ],
+        debug: true,
+    } );
+    
+    b.plugin(tsify, { project: "." } );
+    b.add(inFile);
+    
+    b.on("update", bundle);
+    bundle();
+    
+    function currentTime() {
+        let time = new Date();
+        return `${("0" + time.getHours()).slice(-2)}:${("0" + time.getMinutes()).slice(-2)}:${("0" + time.getSeconds()).slice(-2)}`
+    }
+    
+    function bundle() {
+        let hadError = false;
+        let time = new Date();
+        console.log(`\x1b[32m[${currentTime()}]--- Beginning compilation of ${path.basename(inFile)}\x1b[0m`);
+        b.bundle()
         .on("error", (error) => { console.error(error.message); hadError = true; } )
-        .pipe(fs.createWriteStream(outFile)
-            .on("close", () => {
-                if (hadError) {
-                    console.log(`\x1b[31m[${currentTime()}]    Compilation ended with errors ---\x1b[0m`);
+        .pipe(fs.createWriteStream(path.join(outPath, outFile))
+        .on("close", () => {
+            if (hadError) {
+                console.log(`\x1b[31m[${currentTime()}]    Compiling ${path.basename(inFile)} ended with errors ---\x1b[0m`);
                 } else {
-                    console.log(`\x1b[32m[${currentTime()}]    Compilation complete ---\x1b[0m`);
+                    console.log(`\x1b[32m[${currentTime()}]    Compiling ${path.basename(inFile)} complete ---\x1b[0m`);
                 }
             } ) );
+        }
 }
